313660132437bbe5b0d0e305c3563b52be585df7
Louis Amira <lamira@lamira-macbookpro4.roam.corp.google.com>
2016-02-16 10:34:09 -0500

    Early practice with JS as part of JS Basics on u.co

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Louis Amira")
+
+// var awesomeThoughts = "I am Louis and I am AWESOME";
+
+// console.log(awesomeThoughts);
+
+var name = "Louis Amira";
+var role = "Product Partnerships Manager";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
